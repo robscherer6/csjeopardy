7f7e55615d559060aecf320029b3e9c0ecbda6d5
Rob Scherer <robscherer6@gmail.com>
2023-10-16 06:13:00 -0400

    first commit, coming from school project

---
+++ website/views.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+    return render_template("jeopardy.html")
+
+@views.route('/question/<int:category_id>/<int:point_value>/options')
+def question_options(category_id, point_value):
+    return render_template('question.html')
+
