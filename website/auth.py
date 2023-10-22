dabd3364cdea77bbac9fa1379fb3d0a987d6e354
Rob Scherer <robscherer6@gmail.com>
2023-10-22 13:32:25 -0400

    added redirect to jeopardy page and jeopardy navbar inclusion

--- website/auth.py
+++ website/auth.py
@@ -2,18 +2,35 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
-    return render_template("login.html", boolean=True)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        # searching for user in db
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -25,6 +42,8 @@ def sign_up():
 
         user = User.query.filter_by(email=email).first()
         if user:
+            flash('Email already exists.', category='error')
+        elif user:
             flash('Email already exists.', category='error')
         elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
@@ -39,11 +58,13 @@ def sign_up():
                 password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.jeopardy'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
 
-# @auth.route('/jeopardy')
-# def jeopardy():
-#     return render_template('jeopardy.html')
+@auth.route('/jeopardy', methods=['GET', 'POST'])
+@login_required
+def jeopardy():
+    return render_template('jeopardy.html', user=current_user)
--- website/views.py
+++ website/views.py
@@ -1,15 +1,17 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/')
+@login_required
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user=current_user)
 
 @views.route('/jeopardy')
 def jeopardy():
-    return render_template("jeopardy.html")
+    return render_template("jeopardy.html", user=current_user)
 
 @views.route('/question/<int:category_id>/<int:point_value>/options')
 def question_options(category_id, point_value):
