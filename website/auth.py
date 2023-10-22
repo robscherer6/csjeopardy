7ac8d145079584d61c08e8835dae3fba82170a78
Rob Scherer <robscherer6@gmail.com>
2023-10-22 11:42:24 -0400

    added redirect to jeopardy page after login

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
@@ -18,24 +19,31 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        first_name = request.form.get('firstName')
+        firstName = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
-        elif len(first_name) < 2:
+        elif len(firstName) < 2:
             flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            #add user to database
+            new_user = User(email=email, firstName=firstName, password=generate_password_hash(
+                password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category='success')
+            return redirect(url_for('views.jeopardy'))
 
     return render_template("sign_up.html")
 
-@auth.route('/jeopardy')
-def jeopardy():
-    return render_template('jeopardy.html')
+# @auth.route('/jeopardy')
+# def jeopardy():
+#     return render_template('jeopardy.html')
--- website/views.py
+++ website/views.py
@@ -7,9 +7,9 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html")
 
-# @views.route('/')
-# def home():
-#     return render_template("jeopardy.html")
+@views.route('/jeopardy')
+def jeopardy():
+    return render_template("jeopardy.html")
 
 @views.route('/question/<int:category_id>/<int:point_value>/options')
 def question_options(category_id, point_value):
